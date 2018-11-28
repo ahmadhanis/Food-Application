248a7a55e52baa64dd4518e325ecd65ea6c92613
ahmadhanis <slumberjer@gmail.com>
2018-11-28 13:49:42 +0800

    Initial commit

---
+++ src/Food.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Food {
+    String foodname,foodloc;
+    double foodprice;
+    public Food(String fn,double fp,String fl){
+        this.foodname = fn;
+        this.foodloc = fl;
+        this.foodprice = fp;
+    }
+}
